dbdae54d113bf72179b4f77110cefb193b8566c8
Illarcanuminuim <crystalizedhero@hotmail.com>
2020-12-28 18:17:55 -0800

    Liviathan.splash

---
+++ Leviathan.Splash/FadeInOut.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * gets image and then smoothly interpolates the Alpha 
+ * Value from 0 to 255 then sitson screen for TimeOnScreen amount of seconds
+ * before interpolatign the alpha value back to 0
+ */
+[RequireComponent(typeof(Image))]
+public class FadeInOut : MonoBehaviour
+{
+
+   [Header("Settings")]
+   [Range(.001f,.1f)]
+   [Tooltip("amount of iterations per-second that image will fade into screen" +
+            "the smaller eqauls faster fade in effect")]
+   public float fadeIn = .02f;
+   [Range(.001f, .1f)]
+   [Tooltip("amount of iterations per-second when fading out")]
+   public float fadeOut = .02f;
+   [Range(.001f, 1f)]
+   [Tooltip("the amount at which the image interpolates between values." +
+            "the larger the number the more choppy the fade is")]
+   public float Interpolation = .02f;
+   [Tooltip("the amount of seconds the image speds on screen" +
+             " before begining to fade away")]
+   [Min(1)]
+   public float TimeOnScreen = 5;
+
+   //mutable values
+   Image targetImage;
+   Color Color;
+   float alphaValue;     
+  
+
+    public bool finishedEffect { get; set; }
+
+    //set refrences    
+    void Start()
+    {        
+        targetImage = GetComponent<Image>();       
+        Color = targetImage.color;
+        Color.a = 0;
+        targetImage.color = Color;          
+    }
+
+    public IEnumerator DisplayOnScreen()
+    {
+        finishedEffect = false;
+        
+        StartCoroutine(FadeIn());
+        while (Color.a != 255)
+            yield return new WaitForEndOfFrame();
+
+        yield return new WaitForSeconds(TimeOnScreen);
+        StartCoroutine(FadeOut());        
+    }
+
+
+    IEnumerator FadeIn()
+    {
+        Color.a = 0;
+        alphaValue = 0;
+        while (alphaValue <= 1)
+        {            
+            Color.a = alphaValue;
+            yield return new WaitForSeconds(fadeIn);
+            targetImage.color = Color;
+            alphaValue += .01f;            
+        }
+        Color.a = 255;
+        targetImage.color = Color;
+    }
+
+    IEnumerator FadeOut()
+    {
+        Color.a = 255;
+        alphaValue = 1;
+        while (alphaValue >= 0)
+        {
+            Color.a = alphaValue;
+            yield return new WaitForSeconds(fadeOut);            
+            targetImage.color = Color;
+            alphaValue -= .01f;
+        }
+        Color.a = 0;
+        targetImage.color = Color;
+        finishedEffect = true;
+    }
+}
---
+++ Leviathan.Splash/SplashManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * this class Handles all the settigns of multiple fadeInOut effects objects
+ * and also controls the delay at the start and inbetween each fadeInOut effect 
+ */
+public class SplashManager : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("the fadeInOut effects will play from top to bottom.")]
+    public List<FadeInOut> SplashScreens;
+    [Tooltip("the amount of seconds the cahin of effects will wait" +
+        "before srtating on laod")]
+    public float IntroTime;
+    [Tooltip("the time in seconds it will wait inbetween each efect.")]
+    public float pauseBetween;
+    public bool finishedSequence { get; set; }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        finishedSequence = false;
+        StartCoroutine(IntroWait());
+    }
+
+    public void DisplayInOrder(int index)
+    {
+        if (index > SplashScreens.Count - 1)
+        {           
+            finishedSequence = true;
+        }
+        else
+        {           
+            StartCoroutine(SplashScreens[index].DisplayOnScreen());            
+            StartCoroutine(waitUntilFinished(SplashScreens[index], index));
+        }
+    }
+    IEnumerator waitUntilFinished(FadeInOut Effect, int index)
+    {       
+        while (Effect.finishedEffect is false)
+            yield return new WaitForSeconds(.1f);
+
+        yield return new WaitForSeconds(pauseBetween);       
+        DisplayInOrder(index + 1);          
+    }
+    IEnumerator IntroWait()
+    {
+        yield return new WaitForSeconds(IntroTime);
+        DisplayInOrder(0);
+    }
+}
+
